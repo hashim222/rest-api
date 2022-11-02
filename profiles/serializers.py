79edd98245cd371ade33fa9072a0e82196ddce9e
hashim222 <aslamhashim19@gmail.com>
2022-11-02 17:31:31 +0000

    added user's followers and following count methods

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,18 +1,38 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    followings_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        # print('This is requested user: ', user)
+        # print('This is Object Owners😁: ', obj.owner)
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner,
+            ).first()
+            # print('this is following: ', following)
+            return following.id if following else None
+        else:
+            None
+
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner'
+            'content', 'image', 'is_owner', 'following_id', 'posts_count',
+            'followers_count',
+            'followings_count'
         ]
