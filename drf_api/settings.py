6242780fa043066e8c7b6afcf6fdf75d6a4de7c2
hashim222 <aslamhashim19@gmail.com>
2022-11-10 15:59:55 +0000

    fixed bug in client origin

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -69,7 +69,7 @@ ALLOWED_HOSTS = [
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGIN = [
+    CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
